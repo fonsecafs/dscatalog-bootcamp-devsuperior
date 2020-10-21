5a661b8b81c3bcd66eee6b6a2cb8c8febef08a69
Fabio Fonseca <fonseca.desenv@gmail.com>
2020-10-21 09:57:31 -0300

    Product respository, service, resource, GET ok

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Product.java
@@ -18,7 +18,7 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "tb_product")
 public class Product implements Serializable {
-	private static final long serialVersionUID = 1;
+	private static final long serialVersionUID = 1L;
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
